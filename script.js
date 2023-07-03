a7c87876e1036d680c64650e591d6ea2823b62f6
Kacegz <90635502+Kacegz@users.noreply.github.com>
2023-07-03 15:23:24 +0200

    Change to class and remove comments

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ const booksbox=document.querySelector("#books")
 let books=document.querySelectorAll(".placeable");
 const add=document.querySelector("#addbook");
 add.addEventListener('click',addNewBook);
-addBookToLibrary("Hobbit","Tolkien",310,"Not read yet");
-addBookToLibrary("Witcher","A.Sapkowski",343,"Already read");
 books[0].textContent="";
 let shelfindex=0;
 let counter=0;
-function Book(title,author,pages,read,id){
-    this.title=title;
-    this.author=author;
-    this.pages=pages;
-    this.read=read;
-    this.id=id;
-    this.display=function(){
+class Book{
+    constructor(title,author,pages,read,id){
+        this.title=title;
+        this.author=author;
+        this.pages=pages;
+        this.read=read;
+        this.id=id;
+    }
+    display=function(){
         let book=document.createElement('div');
         book.classList.add("book");
         let title=document.createElement('h1');
@@ -40,9 +40,7 @@ function Book(title,author,pages,read,id){
         change.classList.add("change")
         change.id=this.id;
         change.addEventListener('click',this.changeRead)
-        //book.appendChild(change)
         remove.addEventListener('click',this.removeBook)
-        //book.appendChild(remove)
         choices.appendChild(change);
         choices.appendChild(remove);
         book.appendChild(choices)
@@ -50,7 +48,7 @@ function Book(title,author,pages,read,id){
     }
 
 
-    this.removeBook=()=>{
+    removeBook=()=>{
         console.log(this)
         let shelf = document.querySelector(".placeable")
         console.log(myLibrary[document.getElementById(this)])
@@ -61,7 +59,7 @@ function Book(title,author,pages,read,id){
         }
         delete myLibrary[this.id]
     }
-    this.changeRead=()=>{
+    changeRead=()=>{
         let thisbook=document.getElementById(this.id).getElementsByTagName('p')[1];
         console.log(thisbook)
         if (this.read==="Not read yet"){
@@ -94,11 +92,6 @@ function addNewBook(event){
     }
     document.querySelector("#pages").setAttribute("style","border-color:green")
     addBookToLibrary(newTitle,newAuthor,newPages,newRead,id);
-    //displayNew(newTitle,newAuthor,newPages,newRead,id);
-    /*books.forEach(shelf => {
-        shelf.textContent="";
-    });
-    displayAll();*/
     if(myLibrary.slice(-1)[0].id%4==0){
         shelfindex++;
         let shelf=document.createElement('div');
@@ -124,23 +117,6 @@ function displayAll(){
         myLibrary[book.id].display();
     });
 }
+addBookToLibrary("Hobbit","Tolkien",310,"Not read yet");
+addBookToLibrary("Witcher","A.Sapkowski",343,"Already read");
 displayAll();
-/*
-function displayNew(newtitle,newauthor,newpages,newread,id){
-    let book=document.createElement('div');
-    book.classList.add("book");
-    let title=document.createElement('h1');
-    let author=document.createElement('h2');
-    let pages=document.createElement('p');
-    let read=document.createElement('p');
-    title.textContent=newtitle
-    author.textContent=newauthor
-    pages.textContent=newpages
-    read.textContent=newread
-    book.appendChild(title)
-    book.appendChild(author)
-    book.appendChild(pages)
-    book.appendChild(read)
-    books[shelfindex].appendChild(book);
-    
-}*/
